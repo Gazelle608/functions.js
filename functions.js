d459272bd63ed90fbaf18e061dcdf174aed07038
Gazelle608 <pearsongazelle@gmail.com>
2025-11-03 14:16:56 +0200

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,27 @@
+// TODO: Create a function called greet that takes a name parameter
+function greet(name) {
+    return "Hello, " + name + "!";
+}
+
+// TODO: Make the greet function return "Hello, [name]!"
+function greet(name) {
+    return "Hello, " + name + "!";
+}
+
+// TODO: Create a function called add that takes two parameters
+function add(a, b) {
+    return a + b;
+}
+
+// TODO: Make the add function return the sum of both parameters
+function add(a, b) {
+    return a + b;
+}
+
+// TODO: Call both functions and store their results in variables
+const greeting = greet("Gazelle");
+const sum = add(1520, 9156);
+
+// TODO: Print the results to the console
+console.log(greeting);
+console.log(sum);
